c4e03ba9e7959b0800652f3b3f784688d44638ac
Paul Beattie <Paul@MauleyMedia.co.uk>
2017-08-07 16:03:01 +0100

    Tableview correctly sized and search working on dummy data.

---
+++ searchbar/NetworkHandler.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkHandler.swift
+//  searchbar
+//
+//  Created by Paul Beattie on 07/08/2017.
+//  Copyright © 2017 Paul Beattie. All rights reserved.
+//
+
+import Foundation
---
+++ searchbar/RecipeTVC.swift
@@ -0,0 +1,88 @@
+//
+//  RecipeTVC.swift
+//  searchbar
+//
+//  Created by Paul Beattie on 07/08/2017.
+//  Copyright © 2017 Paul Beattie. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTVC: UIViewController {
+    
+    // The search controller containing doing the filtering
+    var searchController = UISearchController()
+    // The tableview used to display the filtered results.
+    let resultsTableViewController = UITableViewController()
+    
+    @IBOutlet weak var fullListTableView: UITableView!
+    
+    
+    // The raw data
+    let tempResults = ["pie", "custard", "omlete", "boiled Egg", "scrambled egg", "salmon"]
+    // A holder for the filtered results.
+    var filteredRecipeResults = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // The search controller is to use the resultsTableViewController to display its results.
+        searchController = UISearchController(searchResultsController: resultsTableViewController)
+        fullListTableView.tableHeaderView = searchController.searchBar
+        searchController.searchResultsUpdater = self
+        
+        // These need to be added as the this tableview was not added in a storyboard
+        resultsTableViewController.tableView.dataSource = self
+        resultsTableViewController.tableView.delegate = self
+        fullListTableView.dataSource = self
+        fullListTableView.delegate = self
+
+    }
+}
+
+extension RecipeTVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == fullListTableView {
+            return tempResults.count
+        } else {
+            return filteredRecipeResults.count
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView == fullListTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath)
+            cell.textLabel?.text = tempResults[indexPath.row]
+            return cell
+            
+        } else {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = filteredRecipeResults[indexPath.row]
+            return cell
+        }
+        
+    }
+    
+}
+
+extension RecipeTVC: UITableViewDelegate {
+    
+}
+
+extension RecipeTVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredRecipeResults = tempResults.filter({ (stringValue:String) -> Bool in
+            if stringValue.lowercased().contains((searchController.searchBar.text?.lowercased())!) {
+                return true
+            } else {
+                return false
+            }
+        })
+        resultsTableViewController.tableView.reloadData()
+    }
+    
+}
+
